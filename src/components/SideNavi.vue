f1833071ef5b795e8d2f2ffcbc16e2e4c2f98509
tomoatsu <tomoatsu213@gmail.com>
2021-03-09 22:00:28 +0900

    first-commit

---
+++ src/components/SideNavi.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="left-padding">
+    <img src="../assets/logo.png" alt="logo" class="logo" />
+    <div class="flex icon-field">
+      <img src="../assets/home.png" alt="home" class="icon" />
+      <p>ホーム</p>
+    </div>
+    <div class="flex icon-field">
+      <img src="../assets/profile.png" alt="profile" class="icon" />
+      <p>プロフィール</p>
+    </div>
+    <div class="flex icon-field">
+      <img src="../assets/logout.png" alt="logout" class="icon" />
+      <p>ログアウト</p>
+    </div>
+    <ShareMessage />
+  </div>
+</template>
+
+<script>
+import ShareMessage from "../components/ShareMessage";
+export default {
+  components: {
+    ShareMessage,
+  },
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+.left-padding {
+  margin: 20px;
+}
+.logo {
+  width: 100px;
+}
+.icon-field {
+  margin-top: 15px;
+  cursor: pointer;
+}
+.icon-field p {
+  font-size: 16px;
+  padding-left: 15px;
+}
+.icon {
+  width: 25px;
+  height: 25px;
+}
+</style>
