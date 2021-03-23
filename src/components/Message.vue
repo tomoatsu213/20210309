ee96b3f9cda758bbbbc1aaf1e374ec85dd93f817
tomoatsu <tomoatsu213@gmail.com>
2021-03-23 18:23:06 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -4,53 +4,163 @@
       <div class="message">
         <div class="flex">
           <p class="name">{{ value.name }}</p>
-          <img src="../assets/heart.png" alt="heart" class="icon" />
+          <img
+            src="../assets/heart.png"
+            alt="heart"
+            class="icon"
+            @click="fav(index)"
+          />
           <p class="number">{{ value.like.length }}</p>
-          <img src="../assets/cross.png" alt="cross" class="icon" />
-          <img src="../assets/detail.png" alt="detail" class="icon detail" />
+          <img
+            src="../assets/cross.png"
+            alt="cross"
+            class="icon"
+            @click="del(index)"
+            v-if="path && profile"
+          />
+          <img
+            src="../assets/detail.png"
+            alt="detail"
+            class="icon detail"
+            @click="
+              $router.push({
+                path: '/detail/' + value.item.id,
+                params: { id: value.item.id },
+              })
+            "
+            v-if="profile"
+          />
         </div>
-        <p class="text">{{ value.share }}</p>
+        <p class="text">{{ value.item.share }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default({
+import axios from "axios";
+export default {
+  props: ["id"],
   data() {
     return {
-      shares:[{name:"太郎", like:[], share:"初めまして"}]
+      shares: [],
+      path: true,
+      profile: true,
     };
+  },
+  methods: {
+    fav(index) {
+      const result = this.shares[index].like.some((value) => {
+        return value.user_id == this.$store.state.user.id;
+      });
+      if (result) {
+        this.shares[index].like.forEach((element) => {
+          if (element.user_id == this.$store.state.user.id) {
+            axios({
+              method: "delete",
+              url: "https://secret-sea-44927.herokuapp.com/api/like",
+              data: {
+                share_id: this.shares[index].item.id,
+                user_id: this.$store.state.user_id,
+              },
+            }).then((response) => {
+              console.log(response);
+              this.$router.go({
+                path: this.$router.currentRoute.path,
+                force: true,
+              });
+            });
+          }
+        });
+      } else {
+        axios.post("https://secret-sea-44927.herokuapp.com/api/like", {
+          share_id: this.shares[index].item.id,
+          user_id: this.$router.currentRoute.path,
+          force: true,
+        });
+      }
+    },
+    del(index) {
+      axios
+        .delete(
+          "https://secret-sea-44927.herokuapp.com/api/shares/" +
+            this.shares[index].item.id
+        )
+        .then((response) => {
+          console.log(response);
+          this.$router.go({
+            path: this.$router.currentRoute.path,
+            force: true,
+          });
+        });
+    },
+    async getShares() {
+      let data = [];
+      const shares = await axios.get(
+        "https://secret-sea-44927.herokuapp.com/api/shares/"
+      );
+      for (let i = 0; i < shares.data.data.length; i++) {
+        await axios
+          .get(
+            "https://secret-sea-44927.herokuapp.com/api/shares/" +
+              shares.data.data[i].id
+          )
+          .then((response) => {
+            if (this.$route.name == "profile") {
+              if (response.data.item.user_id == this.$store.state.user.id) {
+                data.push(response.data);
+              }
+            } else if (this.$route.name == "detail") {
+              if (response.data.item.id == this.id) {
+                data.push(response.data);
+              }
+            } else {
+              data.push(response.data);
+            }
+          });
+      }
+      this.shares = data;
+      console.log(this.shares);
+    },
+  },
+  created(){
+    if (this.$route.name === "home") {
+      this.path = false;
+    }
+    if (this.$route.name === "detail") {
+      this.profile = false;
+    }
+    this.getShares();
   }
-});
+};
 </script>
 
 <style scoped>
-.flex{
+.flex {
   display: flex;
 }
 .icon {
- width: 25px;
- height: 25px;
+  width: 25px;
+  height: 25px;
 }
 .detail {
- margin-left: 50px;
+  margin-left: 50px;
 }
 .message {
- padding: 20px;
- border-bottom: solid 1px white;
- border-left: solid 1px white;
+  padding: 20px;
+  border-bottom: solid 1px white;
+  border-left: solid 1px white;
 }
 .name {
- font-size: 18px;
- font-weight: bold;
- margin-right: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 10px;
 }
 .text {
- margin-top: 10px;
+  margin-top: 10px;
 }
 .number {
- margin-left: 10px;
- margin-right: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
